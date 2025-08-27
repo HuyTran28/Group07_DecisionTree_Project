Fix tree objective/loss calculation and add result dir creation
Corrected aggregation in Node.getObjective and Node.getLoss to use both left and right children. Added automatic creation of output directories before saving results in exp.py, exp_complexity.py, exp_convergence.py, and exp_gamma.py. Reduced MAX_ITERATION to 1000 in experiment scripts and updated some experiment parameters and commented out runs for clarity.

02_Experiments/source_code/cet.py
@@ -56,13 +56,13 @@ class Node():
         if(self.is_leaf()):
             return self.objective / self.sample.shape[0]
         else:
-            return (self.left.getObjective() + self.left.getObjective()) / self.sample.shape[0]
+            return (self.left.getObjective() + self.right.getObjective()) / self.sample.shape[0]
 
     def getLoss(self):
         if(self.is_leaf()):
             return (self.sample.shape[0] - self.active) / self.sample.shape[0]
         else:
-            return (self.left.getLoss() + self.left.getLoss()) / self.sample.shape[0]
+            return (self.left.getLoss() + self.right.getLoss()) / self.sample.shape[0]
 
     def setIndex(self, idx):
         self.idx = idx
@@ -271,7 +271,7 @@ class CounterfactualExplanationTree():
             node = stack.pop()
             if(node.is_leaf()):
                 if(node.is_infeasible):
-                    return np.inf
+                    return np.inf, 0, 0, 0
                 else:
                     g += node.objective / self.N_; cost += node.cost / self.N_; acts += node.active / self.N_;
                     comp += 1

02_Experiments/source_code/exp.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
@@ -115,6 +116,8 @@ def compare_cv(dataset='g', model='L', n_splits=5):
         print('- Test:'); print('\t- cost: {}'.format(cet_cost)); print('\t- loss: {}'.format(cet_loss)); print('\t- obj.: {}'.format(cet_cost + GAMMA * cet_loss));
         dict_cet['time'].append(cet.time_); dict_cet['n_actions'].append(cet.n_leaves_);
         print()
+        output_dir = f'../results/compare/{model}/convergence'
+        os.makedirs(output_dir, exist_ok=True)
         pd.DataFrame(cet.objs_).to_csv('../results/compare/{}/convergence/cet_{}_objective_{}_{}_{}.csv'.format(model, D.dataset_name, LAMBDA, GAMMA, k), index=False)
 
 
@@ -138,13 +141,17 @@ def compare_cv(dataset='g', model='L', n_splits=5):
     print('- AReS      :', np.mean(dict_ares['n_actions'])); 
     print('- CET       :', np.mean(dict_cet['n_actions']));
     print(); print();
+
+    output_dir = f'../results/compare/{model}'
+    os.makedirs(output_dir, exist_ok=True)
+
     pd.DataFrame(dict_clustering).to_csv('../results/compare/{}/clustering_{}_{}_{}.csv'.format(model, D.dataset_name, LAMBDA, GAMMA), index=False)
     pd.DataFrame(dict_ares).to_csv('../results/compare/{}/ares_{}_{}_{}.csv'.format(model, D.dataset_name, LAMBDA, GAMMA), index=False)
     pd.DataFrame(dict_cet).to_csv('../results/compare/{}/cet_{}_{}_{}.csv'.format(model, D.dataset_name, LAMBDA, GAMMA), index=False)
 
 
 
-MAX_ITERATION = 3000
+MAX_ITERATION = 1000
 COST_TYPE = 'MPS'
 MINSUP = {'g':0.05, 'i':0.05}
 ARES_PARAMS = {'g':
@@ -168,7 +175,7 @@ HARES_PARAMS = {'g':
 
 if(__name__ == '__main__'):
 
-    compare_cv(dataset='i', model='X', n_splits=10)
+    compare_cv(dataset='i', model='X', n_splits=5)
     # compare_cv(dataset='g', model='X', n_splits=10)
     # compare_cv(dataset='i', model='T', n_splits=10)
     # compare_cv(dataset='g', model='T', n_splits=10)

02_Experiments/source_code/exp_complexity.py
@@ -8,6 +8,7 @@ from ares import AReS
 from clustering import Clustering
 from cet import CounterfactualExplanationTree
 
+import os
 
 def compare_comp(dataset='g', model='L', n_actions=[4, 8, 12, 16, 20], lambdas=[0.01, 0.02, 0.03, 0.04, 0.05]):
     np.random.seed(0)
@@ -109,15 +110,14 @@ def compare_comp(dataset='g', model='L', n_actions=[4, 8, 12, 16, 20], lambdas=[
         dict_cet['n_actions'].append(cet.n_leaves_);
         print()
 
+    output_dir = f'../results/complexity/{model}'
+    os.makedirs(output_dir, exist_ok=True)
+
     pd.DataFrame(dict_clustering).to_csv('../results/complexity/{}/clustering_{}_{}.csv'.format(model, D.dataset_name, GAMMA), index=False)
     pd.DataFrame(dict_ares).to_csv('../results/complexity/{}/ares_{}_{}.csv'.format(model, D.dataset_name, GAMMA), index=False)
     pd.DataFrame(dict_cet).to_csv('../results/complexity/{}/cet_{}_{}.csv'.format(model, D.dataset_name, GAMMA), index=False)
 
-
-
-
-
-MAX_ITERATION = 10000
+MAX_ITERATION = 1000
 COST_TYPE = 'MPS'
 MINSUP = {'g':0.05, 'i':0.05}
 ARES_PARAMS = {'g':

02_Experiments/source_code/exp_convergence.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from lightgbm import LGBMClassifier
 from utils import MyTabNetClassifier, DatasetHelper
 from cet import CounterfactualExplanationTree
-
+import os
 
 def convergence(dataset='g', model='L', params=(0.01, 1.0)):
     np.random.seed(0)
@@ -37,11 +37,13 @@ def convergence(dataset='g', model='L', params=(0.01, 1.0)):
     cet = cet.fit(X, max_change_num=3, cost_type=COST_TYPE, C=l, gamma=g, time_limit=60, verbose=True)
     print('## Learned CET')
     cet.print_tree()
+    output_dir = f'../results/convergence/{model}'
+    os.makedirs(output_dir, exist_ok=True)
     pd.DataFrame(cet.objs_).to_csv('../results/convergence/{}/cet_{}_objective_{}_{}.csv'.format(model, D.dataset_name, l, g), index=False)
 
 
 
-MAX_ITERATION = 10000
+MAX_ITERATION = 1000
 COST_TYPE = 'MPS'
 
 if(__name__ == '__main__'):
@@ -50,25 +52,25 @@ if(__name__ == '__main__'):
     convergence(dataset='g', model='L', params=(0.03, 0.75))
     convergence(dataset='g', model='L', params=(0.05, 0.75))
 
-    convergence(dataset='g', model='L', params=(0.01, 1.0))
-    convergence(dataset='g', model='L', params=(0.03, 1.0))
-    convergence(dataset='g', model='L', params=(0.05, 1.0))
+    # convergence(dataset='g', model='L', params=(0.01, 1.0))
+    # convergence(dataset='g', model='L', params=(0.03, 1.0))
+    # convergence(dataset='g', model='L', params=(0.05, 1.0))
 
-    convergence(dataset='g', model='L', params=(0.01, 1.25))
-    convergence(dataset='g', model='L', params=(0.03, 1.25))
-    convergence(dataset='g', model='L', params=(0.05, 1.25))
+    # convergence(dataset='g', model='L', params=(0.01, 1.25))
+    # convergence(dataset='g', model='L', params=(0.03, 1.25))
+    # convergence(dataset='g', model='L', params=(0.05, 1.25))
 
 
-    convergence(dataset='i', model='L', params=(0.01, 0.75))
-    convergence(dataset='i', model='L', params=(0.03, 0.75))
-    convergence(dataset='i', model='L', params=(0.05, 0.75))
+    # convergence(dataset='i', model='L', params=(0.01, 0.75))
+    # convergence(dataset='i', model='L', params=(0.03, 0.75))
+    # convergence(dataset='i', model='L', params=(0.05, 0.75))
 
-    convergence(dataset='i', model='L', params=(0.01, 1.0))
-    convergence(dataset='i', model='L', params=(0.03, 1.0))
-    convergence(dataset='i', model='L', params=(0.05, 1.0))
+    # convergence(dataset='i', model='L', params=(0.01, 1.0))
+    # convergence(dataset='i', model='L', params=(0.03, 1.0))
+    # convergence(dataset='i', model='L', params=(0.05, 1.0))
 
-    convergence(dataset='i', model='L', params=(0.01, 1.25))
-    convergence(dataset='i', model='L', params=(0.03, 1.25))
-    convergence(dataset='i', model='L', params=(0.05, 1.25))
+    # convergence(dataset='i', model='L', params=(0.01, 1.25))
+    # convergence(dataset='i', model='L', params=(0.03, 1.25))
+    # convergence(dataset='i', model='L', params=(0.05, 1.25))
 
 

02_Experiments/source_code/exp_gamma.py
@@ -11,6 +11,7 @@ from sklearn.neighbors import KDTree
 from ce import ActionExtractor
 from utils import DatasetHelper, synthetic_dataset
 
+import os
 
 def sensitivity(dataset='g', model='L', N=10, M=100, gammas=[0.25, 0.5, 0.75, 1.0, 1.25, 1.5], choice='neighbor'):
     np.random.seed(0)
@@ -62,17 +63,17 @@ def sensitivity(dataset='g', model='L', N=10, M=100, gammas=[0.25, 0.5, 0.75, 1.
             res['cost'].append(c); res['loss'].append(l); res['obj'].append(o)
         print()
     print(); print();
+
+    output_dir = f'../results/gamma/{model}'
+    os.makedirs(output_dir, exist_ok=True)
+
     pd.DataFrame(res).to_csv('../results/gamma/{}/sensitivity_{}.csv'.format(model, D.dataset_name), index=False)
-
-
-
-
 
 COST_TYPE = 'MPS'
 
 if(__name__ == '__main__'):
 
     sensitivity(dataset='i', model='L', N=10, M=100, gammas=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-    sensitivity(dataset='g', model='L', N=10, M=100, gammas=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    # sensitivity(dataset='g', model='L', N=10, M=100, gammas=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
     # sensitivity(dataset='i', model='L', N=10, M=100, gammas=[0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0])
     # sensitivity(dataset='g', model='L', N=10, M=100, gammas=[0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0])